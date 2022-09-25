b2b5689b64860bf1795e1ff99f72813852a62174
mferrara625 <mferrara401@yahoo.com>
2022-09-25 01:32:39 -0400

    clicking a piece now shows user available moves, players limited to legal moves only, added turns

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import pawn from './Assets/Pawn.png';
 function App() {
   const [divInfo, setDivInfo] = useState([]);
   const [size, setSize] = useState(8);
-  const [pieceToMove, setPieceToMove] = useState(-1);
-  const [pieceSelected, setPieceSelected] = useState(false); 
+  const [spaceSelected, setSpaceSelected] = useState(-1);
+  const [isPieceSelected, setIsPieceSelected] = useState(false); 
   const [initial, setInitial] = useState(true);
   const [currentPiece, setCurrentPiece] = useState();
+  const [singlePieceMoves, setSinglePieceMoves] = useState([]);
+  const [currentPlayer, setCurrentPlayer] = useState("w");
 
   
 
@@ -39,6 +41,7 @@ function App() {
     console.log(divInfo);
     setDivInfo(tempArray);
   }
+
   const makeBoard = () => {
     divInfo.map((divInfo, index) => {
       return (
@@ -47,61 +50,9 @@ function App() {
     }
     )
   }
-  function handleClick(value) {
-    console.log(value + " was clicked");
-    console.log("CURRENT PIECE TEST: " + currentPiece)
-    if(pieceSelected){
-      if(pieceToMove != value && (divInfo[value].piece == "" || divInfo[value].piece[0] != divInfo[pieceToMove].piece[0])){ // update this to check for if square contains same color piece as player currently moving aka friendly fire
-      updateDivInfo(pieceToMove, value);
-      // document.getElementById(value).style.backgroundImage=`${currentPiece}`;
-      // document.getElementById(value).style.backgroundSize= 'cover';
-      // document.getElementById(value).style.backgroundPosition= "center";
-
-
-      // document.getElementById(pieceToMove).style.backgroundImage="none";
-      }
-      // document.getElementById(pieceToMove).style.backgroundBlendMode= 'soft-light';
-
-      setPieceToMove(-1);
-      setPieceSelected(false);
-      setCurrentPiece(null);
-      return;
-    }
-    if(divInfo[value].piece != ""){
-    setPieceToMove(value);
-    // document.getElementById(value).style.backgroundBlendMode= "color-dodge";
-    setPieceSelected(true);
-    setCurrentPiece(document.getElementById(value).style.backgroundImage);
-    console.log("IMAGE TEST: " + document.getElementById(value).style.backgroundImage);
-
-    }
-  }
-  function updateDivInfo(oldNum, newNum){
-    let tempArray = divInfo;
-    let tempObject = divInfo[oldNum];
-    tempArray[oldNum] = { name: (tempArray[oldNum].name), row: (tempArray[oldNum].row), column: (tempArray[oldNum].column), piece: "" }
-    tempArray[newNum] = { name: (tempArray[newNum].name), row: (tempArray[newNum].row), column: (tempArray[newNum].column), piece: (tempObject.piece) }
-
-    setDivInfo(tempArray);
-  }
-
-  function randomizePawns(){
-    let tempArray = divInfo;
-    for(let i = 0 ; i < tempArray.length; i++){
-      if(Math.floor(Math.random() * 2) == 1)
-      tempArray[i] = { name: (tempArray[i].name), row: (tempArray[i].row), column: (tempArray[i].column), piece: "wp" }
-      else
-      tempArray[i] = { name: (tempArray[i].name), row: (tempArray[i].row), column: (tempArray[i].column), piece: "" }
-
-
-    }
-    setDivInfo(tempArray);
-
-    console.log("TEST RANDO");
-
-  }
 
   function createDivs(html, name, index, row, col, piece){
+    console.log("TEST AVAILABLE MOVES: " + singlePieceMoves);
     // console.log("R/C TEST: " + row + " " + col)
       const ele = document.createElement('div');
       ele.id = index;
@@ -118,12 +69,13 @@ function App() {
       else {
       ele.innerHTML = html;
       }
-
-      if(pieceToMove == index)
+      if(singlePieceMoves.includes(index)){
+        ele.style.backgroundColor = "#228B22";
+      }
+      if(spaceSelected == index)
       ele.style.border="dashed";
       else
       ele.style.border="none";
-
 
 
       ele.style.backgroundBlendMode="soft-light";
@@ -142,6 +94,87 @@ function App() {
       return ele;
   }
 
+  function handleClick(value) {
+    console.log(value + " was clicked");
+    console.log("CURRENT PIECE TEST: " + currentPiece)
+    if(isPieceSelected){
+      if(spaceSelected != value && (divInfo[value].piece == "" || divInfo[value].piece[0] != divInfo[spaceSelected].piece[0]) && singlePieceMoves.includes(value)){ // update this to check for if square contains same color piece as player currently moving aka friendly fire
+      updateDivInfo(spaceSelected, value);
+      flipBoard();
+      }
+      setSinglePieceMoves([]);
+      setSpaceSelected(-1);
+      setIsPieceSelected(false);
+      setCurrentPiece(null);
+      return;
+    }
+    if(divInfo[value].piece != "" && divInfo[value].piece[0] == currentPlayer){
+    findAvailableMoves(value);
+    setSpaceSelected(value);
+    setIsPieceSelected(true);
+    setCurrentPiece(document.getElementById(value).style.backgroundImage);
+    console.log("IMAGE TEST: " + document.getElementById(value).style.backgroundImage);
+
+    }
+  }
+
+  function updateDivInfo(oldNum, newNum){
+    let tempArray = divInfo;
+    let tempObject = divInfo[oldNum];
+    tempArray[oldNum] = { name: (tempArray[oldNum].name), row: (tempArray[oldNum].row), column: (tempArray[oldNum].column), piece: "" }
+    tempArray[newNum] = { name: (tempArray[newNum].name), row: (tempArray[newNum].row), column: (tempArray[newNum].column), piece: (tempObject.piece) }
+
+    setDivInfo(tempArray);
+  }
+
+  function findAvailableMoves(spaceNum){
+    let tempArray = [];
+    if(divInfo[spaceNum].piece[1] == "p"){
+      if(divInfo[spaceNum - 8].piece == "")
+      tempArray.push(spaceNum - 8);
+      if(spaceNum > 48){
+        tempArray.push(spaceNum - 16);
+      }
+      if(divInfo[spaceNum - 7].piece[0] != divInfo[spaceNum].piece[0] && divInfo[spaceNum - 7].piece != ""){ // will need to upadte this to account for enemy regardless of color
+        tempArray.push(spaceNum - 7);
+      }
+      if(divInfo[spaceNum - 9].piece[0] != divInfo[spaceNum].piece[0] && divInfo[spaceNum - 9].piece != ""){ // will need to upadte this to account for enemy regardless of color
+        tempArray.push(spaceNum - 9);
+      }
+      setSinglePieceMoves(tempArray);
+    }
+  }
+
+  function flipBoard(){
+    let tempArray = [...divInfo];
+    tempArray.reverse();
+    setDivInfo(tempArray);
+    console.log("divInfo 1:" + divInfo[12].piece);
+    console.log("divInfo 2:" + divInfo.reverse()[12].piece);
+    console.log("CURRENT PLAYER: " + currentPlayer);
+    if(currentPlayer == "w")
+      setCurrentPlayer("b")
+    else
+      setCurrentPlayer("w");
+
+  }
+  function flipView(){
+    // TODO: create method to see the board from blacks perspective
+  }
+
+  function randomizePawns(){
+    let tempArray = divInfo;
+    for(let i = 0 ; i < tempArray.length; i++){
+      if(Math.floor(Math.random() * 2) == 1)
+      tempArray[i] = { name: (tempArray[i].name), row: (tempArray[i].row), column: (tempArray[i].column), piece: "wp" }
+      else
+      tempArray[i] = { name: (tempArray[i].name), row: (tempArray[i].row), column: (tempArray[i].column), piece: "" }
+
+    }
+    setDivInfo(tempArray);
+
+  }
+
   useEffect(() => {
     setDivInfo([])
     document.querySelector(".App-chessboard").innerHTML = "";
@@ -152,7 +185,7 @@ function App() {
     document.querySelector(".App-chessboard").innerHTML = "";
 
     makeBoard()
-  }, [divInfo, pieceSelected, pieceToMove, currentPiece])
+  }, [divInfo, isPieceSelected, spaceSelected, currentPlayer])
 
   return (
     <div style={{ margin: "200px", justifyContent: "center" }}>
@@ -165,6 +198,8 @@ function App() {
 
       </div>
     <button style={{}} onClick={() => randomizePawns()}>DO NOT CLICK</button>
+    <button style={{}} onClick={() => flipBoard()}>Flip Board</button>
+
     </div>
   );
 }
